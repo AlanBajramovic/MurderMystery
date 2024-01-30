f3ea045fc3c1d6762d172898053e43342a64fef3
anxy <143069429+AlanBajramovic@users.noreply.github.com>
2024-01-30 18:18:21 +0100

    MurderMystery.py

--- MurderMystery.py
+++ MurderMystery.py
@@ -43,7 +43,7 @@ def door_task_1():
         answer = input("Ditt svar (biffig/skinny):").lower()
 
         if answer == "biffig":
-            print("Rätt svar! Fortsätt till nästa fråga.")
+            print("Rätt svar. Du har klarat uppgifter till dörr 1 återgår till menyn!")
         else:
             print("Fel svar! Du förlorar ett liv.")
             attempts = attempts - 1
@@ -89,26 +89,29 @@ def door_task_3():
             attempts = attempts - 1
         break
 
+    # Återställ uppgifter för nästa fråga
+    tasks = list(correct_answers)
+
     attempts = 3
     while attempts > 0 and tasks:
         task = tasks.pop(0)
         print(f"Uppgift: {task}")
         answer = input("Ditt svar (yxa/kniv):").lower()
         if answer == "kniv":
-            print("Rätt svar! Fortsätt till nästa fråga.")
+            print("Rätt svar, får uppgifter för att hitta mördarens plats..")
         else:
             print("Fel svar! Du förlorar ett liv.")
             attempts = attempts - 1
         break
-    # Loopa igenom uppgifter och svar för de två följande frågorna
-    
-    # Slutligen, bedöm om spelaren har klarat uppgifterna
-    if attempts > 3:
+
+    # Bedöm om spelaren har klarat uppgifterna
+    if attempts > 0:
         print("Bra jobbat! Du har hittat mördaren. Du får ett vapen!")
         return True
     else:
         print("Du förlorade alla dina liv. Spelet är över.")
         return False
+    # Slutligen, bedöm om spelaren har klarat uppgifterna
 
 # Huvudfunktionen
 def main():
@@ -131,7 +134,7 @@ def main():
         elif answer == "3":
             door_3_result = door_task_3()
             if door_3_result: 
-                print("Grattis, du har fångat mördaren och här får du ett vapen!")
+                print("Grattis, du har klarat av spelet, tack för att du spelar Murder Mystery!")
                 break
         else:
             print("Ogiltigt val. Försök igen.")
